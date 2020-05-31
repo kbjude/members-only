d19d49d40ebfae6f22f2cfb717845d62d4cf6761
kbjude <kbjude@gmail.com>
2020-05-31 17:58:42 +0300

    index and root route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+    def index
+        @post = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        # @post = Post.new
+    end
+
+    private
+
+    def post_params
+        params.require(post).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :user
+  resources :post, only: %i[:new, :create, :index]
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
