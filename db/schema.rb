dae25bb624eda4ae7cfc5d4672bd8857bd89dcd8
kbjude <kbjude@gmail.com>
2020-06-01 00:17:18 +0300

    WIP index

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_142426) do
+ActiveRecord::Schema.define(version: 2020_05_31_210351) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_posts_on_user_id_id"
   end
 
   create_table "users", force: :cascade do |t|
