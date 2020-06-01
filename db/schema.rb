912cc36ac441293d80bbcc66d1f904aca107cea3
kbjude <kbjude@gmail.com>
2020-06-01 09:14:40 +0300

    devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_210351) do
+ActiveRecord::Schema.define(version: 2020_06_01_061317) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,12 +21,15 @@ ActiveRecord::Schema.define(version: 2020_05_31_210351) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
