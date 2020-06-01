7a49be7d8b8ba82d6f57d88d84d414803e8a62af
kbjude <kbjude@gmail.com>
2020-06-01 20:26:57 +0300

    current user effected

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,24 @@
 class PostsController < ApplicationController
+    #before_action :set_post!, only: %i[new create]
+    before_action :authenticate_user!, except: [:index]
     def index
-        @posts = Post.all
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
 
     def create
-        # @post = Post.new
+        @post = current_user.posts.build(post_params)
+        if @post.save!
+            redirect_to posts_path
+        end
     end
 
     private
 
     def post_params
-        params.require(post).permit(:title, :body)
+        params.require(:post).permit(:title, :body)
     end
 end
