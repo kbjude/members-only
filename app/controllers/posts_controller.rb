ee15856bec8e643fac208007b5c6e48facc1bbb3
kbjude <kbjude@gmail.com>
2020-06-02 15:52:10 +0300

    linters and feedback on post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,26 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, except: [:index]
-    before_action :set_post, only: %i[new create]
-    def index
-        @posts = Post.all.order("created_at DESC")
-    end
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_post, only: %i[new create]
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
-    def new
-        @post = current_user.post.build
-    end
+  def new
+    @post = current_user.posts.build
+  end
 
-    def create
-        @post = current_user.post.build(post_params)
-        if @post.save!
-            redirect_to posts_path
-        end
-    end
+  def create
+    @post = current_user.posts.build(post_params)
+    redirect_to posts_path if @post.save!
+  end
 
-    private
+  private
 
-    def set_post
-        @post = Post.new
-    end
+  def set_post
+    @post = Post.new
+  end
 
-    def post_params
-        params.require(:post).permit(:title, :body)
-    end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
